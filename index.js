500b5a2a6e1e92d67c874f3cec5828b9dde91ae3
wasiqafnan <wansari964@yahoo.com>
2025-07-14 13:23:20 +0530

    Add prettier setup and gitignore

--- index.js
+++ index.js
@@ -1,27 +1,4 @@
-const express = require('express');
+import express from 'express';
 const app = express();
-const mongoose = require('mongoose');
-
-// Import Models
-require('./models/user'); // this line showing error
-require('./models/Recipe');
-require('./models/NutritionEntry');
-require('./models/Subscription');
-
-const MONGO_URL = "mongodb://127.0.0.1:27017/bitebot";
-
-main()
-    .then(() => {
-        console.log("Connected to Database bitebot");
-    })
-    .catch((err) => {
-        console.log(err);
-    });
-
-async function main() {
-    await mongoose.connect(MONGO_URL);
-}
-
-app.get('/', (req, res) => res.status(200).json({ message: "Welcome to API of BiteBot" }));
 
 app.listen(8080, () => console.log("Server Running"));
