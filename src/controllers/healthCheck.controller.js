92a99ae9309cabf14567ea92215fb6de0e103752
wasiqafnan <wansari964@yahoo.com>
2025-08-07 19:05:53 +0530

    Add ApiResponse and ApiError class

--- src/controllers/healthCheck.controller.js
+++ src/controllers/healthCheck.controller.js
@@ -1,5 +1,4 @@
+import {ApiResponse} from '../utils/index.js'
 export const handleHealthCheck = (req, res) => {
-    return res.status(200).json({
-        message: "Server is up and running"
-    });
+    return res.status(200).json(new ApiResponse(200, "Server is up and running"));
 };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,4 +1,8 @@
 import { Router } from "express";
+import { handleRegister, handleLogin } from "../controllers/user.controllers.js";
 const userRoutes = Router();
+
+userRoutes.route("/register").post(handleRegister);
+userRoutes.route("/login").post(handleLogin);
 
 export default userRoutes;
